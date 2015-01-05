2d742b94490104954cdd50fcf96cf9f2c8965236
Robert Landreaux <rcl2288@yahoo.com>
2015-01-05 12:43:09 -0500

    modify root route, add 'view created students link' to courses/index.html.erb, and more

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,7 +1,7 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, except: [:index, :show]
-
+  before_action :authorized_user, only: [:edit, :update, :destroy]
   respond_to :html
 
   def index
@@ -10,6 +10,7 @@ class CoursesController < ApplicationController
   end
 
   def show
+    @assignments = Assignment.all
     respond_with(@course)
   end
 
@@ -48,5 +49,10 @@ class CoursesController < ApplicationController
 
     def course_params
       params.require(:course).permit(:name, :time, :period, :user, :section)
+    end
+
+    def authorized_user
+      @course = current_user.courses.find_by(id: params[:id])
+      redirect_to courses_path, notice: "Not authorized to edit this course" if @course.nil?
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Gradejoy::Application.routes.draw do
 
   devise_for :users
 
-  root "pages#index"
+  root "courses#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
