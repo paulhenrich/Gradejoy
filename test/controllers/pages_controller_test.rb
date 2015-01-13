ee3191576f8340c38418805ed00ed289bc564ddb
Robert Landreaux <rcl2288@yahoo.com>
2015-01-12 23:22:58 -0500

    edit and delete tests, add course_enrollments ui

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -26,11 +26,6 @@ class PagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-  end
-
   test "should get account" do
     get :account
     assert_response :success
