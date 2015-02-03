f8c3826d0c6372e5187279170a18f3a623c67fb1
robertlandreaux <rcl2288@yahoo.com>
2015-02-03 14:03:15 -0500

    attempt to fix ajax duplicates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_filter :configure_permitted_parameters, if: :devise_controller?
   respond_to :html, :js
-  
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u|
       u.permit(:password, :password_confirmation, :current_password)
