d4e9762b73bc2d45d5237d2c7de6ed65655e722b
robertlandreaux <rcl2288@yahoo.com>
2015-02-04 20:50:43 -0500

    users :confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable,  :confirmable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:digitalocean]
 
  def self.from_omniauth(auth)
@@ -19,4 +19,5 @@ class User < ActiveRecord::Base
   has_many :course_assignments
   has_many :course_enrollments
   has_many :grades
+
 end
